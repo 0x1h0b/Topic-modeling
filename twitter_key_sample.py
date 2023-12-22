f1a642cc8c41c7aeba45fc6dd2da857026a20dd6
Himanshu Bag <himanshubag12@gmail.com>
2023-12-23 01:51:49 +0530

    sample/fake twitter keys

---
+++ twitter_key_sample.py
@@ -0,0 +1,6 @@
+consumer_key = '<your-key>'
+consumer_secret = '<your-key>'
+access_token = '<your-key>'
+access_token_secret = '<your-key>'
+
+# create a new file twitter_key_main.py , save your real keys
